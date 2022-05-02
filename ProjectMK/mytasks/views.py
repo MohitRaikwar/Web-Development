52a0e511d86d1754d1b2e833566b34459a60248a
Mohit Kumar <vegasgangstar25@gmail.com>
2022-05-02 12:44:10 +0530

    Django Basics Complete with some examples

---
+++ ProjectMK/mytasks/views.py
@@ -0,0 +1,33 @@
+from enum import auto
+from logging import PlaceHolder
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+class NewTaskForm(forms.Form):
+    task=forms.CharField(label="New Task")
+    priority=forms.IntegerField(label="Priority",min_value=1,max_value=5)
+
+# Create your views here.
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
+    return render(request,"mytasks/index.html",{
+        "tasks":request.session["tasks"]
+    })
+
+def add(request):
+    if request.method=="POST":
+        form=NewTaskForm(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            request.session["tasks"]+=[task]
+            return HttpResponseRedirect(reverse("mytasks:index"))
+        else:
+            return render(request,"mytasks/add.html",{
+                "form":form
+            })
+    return render(request,"mytasks/add.html",{
+        "form":NewTaskForm()
+        })
